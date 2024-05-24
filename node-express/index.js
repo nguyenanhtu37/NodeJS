029ca488a32d35763482120ea1331cc3d02d4c9d
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2024-05-24 10:45:53 +0700

    Express Example

--- node-express/index.js
+++ node-express/index.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+    // res.write("Hello world");
     res.end(`
         <html>
             <body>
