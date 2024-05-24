6a223c826d025b5e61abeef80196ad4e1d843585
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2024-05-24 13:04:58 +0700

    Express Simple REST

--- node-express/index.js
+++ node-express/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
-
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -28,6 +28,49 @@ app.use(express.static(__dirname + '/public'));
 /*
 sử dụng middleware 'express.static' để phục vụ các tệp tĩnh từ thư mục public trong thư mục hiện tại '__dirname"
 */
+
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.end('PUT operation is not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('dishes/:dishId', (req, res, next) => {
+    res.statusCode = 200;
+    res.end('POST operation is not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 const server = http.createServer(app);
 
