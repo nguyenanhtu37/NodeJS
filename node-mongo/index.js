42acd43501de8e50c9c8601e394dcd3ce0173f13
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2024-06-04 10:16:48 +0700

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,37 +1,70 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient; // Client sử dụng để kết nối đến máy chủ
+const assert = require('assert'); // module thực thiện kiểm tra xác nhận, đảm bảo mã hoạt động đung như mong đợi
+
+const dboperation = require('./operations');
 
 const url = 'mongodb://127.0.0.1:27017';
-const dbname = 'conFunsion';
+const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+    /*
+    MongoClient.connect(url, (callback)): Kết nối đến MongoDB sử dụng url đã định nghĩa, hàm callback được gọi sau khi kết nối thành công hoặc thất bại
+    */
 
-    assert.equal(err, null);
+    assert.equal(err, null); // check if err == null ???
 
     console.log('Connected correctly to server');
 
-    const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({ "name": "Pizza", "description": "Pizza Hub" },
-        (err, result) => {
-            assert.equal(err, null);
+    const db = client.db(dbname); // truy cap csdl
+    const collection = db.collection("dishes"); // truy cap collection
 
-            console.log("After Insert:\n");
-            console.log(result.ops);
+    /**
+     * collection.insertOne(document, callback):
+     * insert document to collection
+     * callback func will be called after insert successfully or failed
+     */
+    // collection.insertOne({ "name": "Pizza", "description": "Pizza Hub" },
+    //     (err, result) => {
+    //         assert.equal(err, null);
 
-            collection.find({}).toArray((err, docs) => {
-                assert.equal(err, null);
+    //         console.log("After Insert:\n");
+    //         console.log(result.ops); // print out all documents were inserted
 
-                console.log("Found:\n");
-                console.log(docs);
+    //         collection.find({}).toArray((err, docs) => {
+    //             assert.equal(err, null);
 
-                // db.dropCollection("dishes", (err, result) => {
-                //     assert.equal(err, null);
+    //             console.log("Found:\n");
+    //             console.log(docs);
 
-                //     client.close();
-                // });
+    //             // db.dropCollection("dishes", (err, result) => {
+    //             //     assert.equal(err, null);
 
-                client.close();
+    //             //     client.close();
+    //             // });
+
+    //             client.close();
+    //         });
+    //     });
+
+    dboperation.insertDocument(db, {name: "Vadonut", description: "Test"}, "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
+
+        dboperation.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
+
+            dboperation.updateDocument(db, {name: "Vadonut"}, {description: "Updated Test"}, "dishes", (result) => {
+                console.log("Updated Document:\n", result.result);
+
+                dboperation.findDocuments(db, "dishes", (docs) => {
+                    console.log("Found Updated Documents:\n", docs);
+
+                    db.dropCollection('dishes', (result) => {
+                        console.log("Dropped Collection: ", result);
+
+                        client.close();
+                    });
+                });
             });
         });
+    });
 });
