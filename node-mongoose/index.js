9febfcbbb49782ae6e0a460a5564367e1de0360d
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2024-06-04 11:37:56 +0700

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,35 +1,51 @@
 const mongoose = require('mongoose');
-
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://127.0.0.1:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // Chèn và xử lý tài liệu 'Orange'
+    Dishes.create({
         name: 'Orange',
         description: 'test'
-    });
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
 
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        console.log("Collection cleared");
 
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        // Chèn và xử lý tài liệu 'Uthapizza'
+        return Dishes.create({
+            name: 'Uthapizza',
+            description: 'Test'
         });
+    })
+    .then((dish) => {
+        console.log(dish);
 
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        console.log("Collection cleared");
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
